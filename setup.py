a7d9e08e084372f82e556b5ae73c503eec3c0cec
azane <azane@nnu.edu>
2019-05-05 12:41:56 -0400

    adds hat_dot_pow3 function in cython.

--- setup.py
+++ setup.py
@@ -2,5 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='Hello world app',
-      ext_modules=cythonize("src/*.pyx"),
-      annotate=True)
+      ext_modules=cythonize("src/*.pyx", annotate=True))
--- src/hats.pyx
+++ src/hats.pyx
@@ -1,2 +1,40 @@
+
+
 cpdef say_hello_to(name):
-    print("Hello %s!" % name)
+    print("Helloo %s!" % name)
+
+
+cpdef double hat(double x, size_t i_, size_t n_) except *:
+    cdef:
+        double i = <double>i_
+        double np1 = <double>(n_+1)
+
+    if x < ((i-1)/np1): return 0
+    if x > ((i+1)/np1): return 0
+
+    if x < (i/np1):
+        return np1*x - i + 1
+    if x > (i/np1):
+        return i - np1*x + 1
+
+
+cpdef double hat_dot_pow3(double x, double[:] w, size_t n) except *:
+    cdef:
+        double s = 0
+        size_t i, j, k
+
+    for i in range(n):
+        for j in range(i-1, i+2):
+            if 0 > j or j >= n:
+                continue
+            if i == j:
+                for k in range(i-1, i+2):
+                    if 0 > k or k >= n:
+                        continue
+                    s += w[i] * w[j] * w[k] * hat(x, i, n) * hat(x, j, n) * hat(x, k, n)
+            else:
+                # for k in (i, j)
+                s += w[i]**2 * w[j] * hat(x, i, n)**2 * hat(x, j, n)
+                s += w[j]**2 * w[i] * hat(x, j, n)**2 * hat(x, i, n)
+
+    return s
