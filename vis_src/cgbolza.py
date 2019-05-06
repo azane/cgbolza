6c10ec5d061dbee29d839cfaf898c804da6b0ea6
azane <azane@nnu.edu>
2019-05-05 20:35:47 -0400

    fleshes out some visualizations.

--- vis_src/cgbolza.py
+++ vis_src/cgbolza.py
@@ -39,10 +39,45 @@ def Ifp(w_, h_, hp_):
 
 
 if __name__ == "__main__":
-    n = 40
-    xx = np.linspace(0, 1, 3000)
+    n = 30
+    xx = np.linspace(0, 1, 1000)
     h, hp = hhp(xx, n)
     w = np.random.normal(size=(n,))
+
+    if n == 2 and len(xx) <= 500:
+        delta = 0.007
+        w1 = np.arange(-1., 1., delta)
+        w2 = np.arange(-1., 1., delta)
+        W1, W2 = np.meshgrid(w1, w2)
+
+        W = np.vstack((W1[None, ...], W2[None, ...])).transpose(1, 2, 0)[:, :, None, :]
+        u = (W * h[None, None, ...]).sum(-1)
+        up = (W * h[None, None, ...]).sum(-1)
+        F = np.log(((up ** 2 - 1) ** 2 + u ** 4).sum(-1))
+
+        _, ax = plt.subplots()
+        CS = ax.contourf(W1, W2, F)
+        plt.colorbar(CS)
+        ax.set_title('Contours of Functional, 2 Parameters')
+        ax.set_xlabel('w1')
+        ax.set_ylabel('w2')
+        plt.show()
+
+        exit()
+
+    # <Contours for u and u'>
+    delta = 0.005
+    U_ = np.arange(-1.5, 1.5, delta)
+    XI_ = np.arange(-1.5, 1.5, delta)
+    U, XI = np.meshgrid(U_, XI_)
+    F = np.log(((XI ** 2 - 1) ** 2 + U ** 4) + 1)
+    _, ax = plt.subplots()
+    CS = ax.contourf(U, XI, F)
+    plt.colorbar(CS)
+    ax.set_title('Log Contours of Functional')
+    ax.set_xlabel('u')
+    ax.set_ylabel('xi')
+    # <Contours for u and u'>
 
     # plt.figure()
     # plt.plot(h)
@@ -64,21 +99,32 @@ if __name__ == "__main__":
     # plt.show()
 
     plt.ion()
-    plt.figure()
-    plt.xlabel("x")
+    fig, uax = plt.subplots()
+    ucolor = 'tab:blue'
+
+    fax = uax.twinx()
+    fcolor = 'tab:orange'
 
     err = []
     gnorm = []
 
     def callback(w_):
-        plt.clf()
-        u = uu(w_, h)
-        plt.plot(u, label="u(x)")
-        plt.plot(np.log(f(w_, h, hp) + 1), label="log(f(u', u))")
-        plt.legend()
-        plt.pause(0.01)
+
+        uax.clear()
+        uax.set_xlabel('x')
+        uax.set_ylabel('u(x)', color=ucolor)
+        uax.tick_params(axis='y', labelcolor=ucolor)
+        uax.plot(xx, uu(w_, h), color=ucolor)
+
+        fax.clear()
+        fax.set_ylabel("f(u', u)", color=fcolor)
+        fax.tick_params(axis='y', labelcolor=fcolor)
+        fax.plot(xx, f(w_, h, hp), color=fcolor)
+
         err.append(If(w_, h, hp))
         gnorm.append(np.linalg.norm(Ifp(w_, h, hp)))
+
+        plt.pause(0.01)
 
 
     fmin_cg(f=lambda w_: np.log(If(w_, h, hp)),
@@ -86,8 +132,7 @@ if __name__ == "__main__":
             x0=w,
             disp=True,
             callback=callback,
-            gtol=0.1,
-            maxiter=120,
+            maxiter=200,
             norm=2)
 
     plt.ioff()
@@ -104,3 +149,5 @@ if __name__ == "__main__":
     plt.xlabel("Iterations")
     plt.plot(gnorm)
     plt.show()
+
+
